e54a822e97b41af07543669bb5e111746bb8f1af
197153 <197153@B466LCOK>
2024-01-06 16:32:37 +0530

    6/1/24

---
+++ CaseStudy/Utilities/DataRead.cs
@@ -0,0 +1,53 @@
+﻿using CaseStudy.TestDataClasses;
+using ExcelDataReader;
+using System.Data;
+using System.Text;
+
+namespace CaseStudy.Utilities
+{
+    public class DataRead
+    {
+        public static List<AmazonText> AmazonReadData(string excelFilePath, string? sheetName)
+        {
+            List<AmazonText> excelDataList = new List<AmazonText>();
+            Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var stream = File.Open(excelFilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                        var result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                        {
+                            ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                            {
+                                UseHeaderRow = true,
+                            }
+                        });
+                        var dataTable = result.Tables[sheetName];
+                        if (dataTable != null)
+                        {
+
+                            foreach (DataRow row in dataTable.Rows)
+                            {
+                                AmazonText excelData = new AmazonText
+                                {
+                                    SearchText = GetValueOrDefault(row, "searchtext")
+
+                                };
+                                excelDataList.Add(excelData);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Sheet '{sheetName}' not found in the excel file");
+                        }
+                    }
+                }
+            return excelDataList;
+        }
+        static string? GetValueOrDefault(DataRow row, string columnName)
+        {
+            Console.WriteLine(row + "  " + columnName);
+            return row.Table.Columns.Contains(columnName) ? row[columnName]?.ToString() : null;
+        }
+    }
+}
